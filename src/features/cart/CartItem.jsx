9d7bbac306a2815164d23212cb7bbd4318462377
Luis Rodriguez <luis.rodriguez.simancas@gmail.com>
2023-10-12 23:29:11 -0500

    first commit

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import { formatCurrency } from "../../utils/helpers";
+import DeleteItem from "./DeleteItem";
+import UpdateItemQuantity from "./UpdateItemQuantity";
+import { getCurrentQuantityById } from "./cartslice";
+
+function CartItem({ item }) {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
+
+  return (
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
+        {quantity}&times; {name}
+      </p>
+      <div className="sn:gap-6 flex items-center justify-between gap-3 ">
+        <p className="mx-5 text-sm font-bold">{formatCurrency(totalPrice)}</p>
+
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        ></UpdateItemQuantity>
+        <DeleteItem pizzaId={pizzaId} />
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
